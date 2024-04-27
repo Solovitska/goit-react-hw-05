0b2c7f2d6314db706e1a6d1e4551ce5fd6ff3ec2
unknown <tanish@ua.i>
2024-04-27 22:34:22 +0300

    up

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,31 +1,34 @@
-import Navigation from '../Navigation/Navigation';
-import Loader from '../Loader/Loader';
-import MovieCast from '../MovieCast/MovieCast';
-import MovieReviews from '../MovieReviews/MovieReviews';
-import { Route, Routes } from 'react-router-dom';
-import { Suspense } from 'react';
-import HomePage from '../../pages/HomePage/HomePage';
-import MoviesPage from '../../pages/MoviesPage/MoviesPage';
-import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
-import MovieDetailsPage from '../../pages/MovieDetailsPage/MovieDetailsPage';
 
-const App = () => {
+import Navigation from '../Navigation/Navigation'
+import Loader from '../Loader/Loader'
+import MovieCast from '../MovieCast/MovieCast'
+import MovieReviews from '../MovieReviews/MovieReviews'
+import { Route, Routes } from 'react-router-dom'
+import { lazy, Suspense } from 'react'
+
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage'))
+const MoviesPage = lazy(() => import('../../pages/MoviesPage/MoviesPage'))
+const NotFoundPage = lazy(() => import('../../pages/NotFoundPage/NotFoundPage'))
+const MovieDetailsPage = lazy(() => import('../../pages/MovieDetailsPage/MovieDetailsPage'))
+
+export default function App() {
+
+
     return (
-        <>
+        <div className="container">
             <Navigation />
+
             <Suspense fallback={<Loader />}>
                 <Routes>
                     <Route path='/' element={<HomePage />} />
                     <Route path='/movies' element={<MoviesPage />} />
                     <Route path='/movies/:movieId' element={<MovieDetailsPage />}>
-                        <Route path='cast' element={<MovieCast />} />
-                        <Route path='reviews' element={<MovieReviews />} />
+                        <Route path='cast' element={<MovieCast />}/>
+                        <Route path='reviews' element={<MovieReviews />}/>
                     </Route>
                     <Route path='*' element={<NotFoundPage />} />
                 </Routes>
             </Suspense>
-        </>
-    );
-};
-
-export default App;
+        </div>
+    )
+}
